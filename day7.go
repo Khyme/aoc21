baeb321b00b5e47af4bbc3a0ef0cee3d787692bb
Kim Schuhmacher <kim@timescale.com>
2021-12-07 22:30:09 +0100

    Day 7 actually its the mean

--- day7.go
+++ day7.go
@@ -9,20 +9,32 @@ import (
 func crabs(start []int) int {
 	sort.Ints(start)
 	tot := len(start)
+	sum := 0
+	med := 0
+	for i := 0; i < tot; i++ {
+		sum += start[i]
+		if i == tot/2 {
+			med = start[i]
+		}
+	}
+	medF := float64(med)
+	meanF := math.Round(float64(sum) / float64(tot))
 	min1 := math.MaxInt
 	min2 := math.MaxInt
-
-	for j := start[0]; j <= start[tot-1]; j++ {
-		val1 := fuel(start, j)
+	var j1, j2 float64
+	for j := math.Min(medF, meanF); j <= math.Max(medF, meanF); j++ {
+		val1 := fuel(start, int(j))
 		if val1 < min1 {
+			j1 = j
 			min1 = val1
 		}
-		val2 := fuel2(start, j)
+		val2 := fuel2(start, int(j))
 		if val2 < min2 {
+			j2 = j
 			min2 = val2
 		}
 	}
-	fmt.Printf("%v %v\n", min1, min2)
+	fmt.Printf("%v/%v %v/%v\n", j1, min1, j2, min2)
 	return min1
 }
 
